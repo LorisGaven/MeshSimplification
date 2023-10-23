b00fae35d889981534e29d1089a19e235a8a89ea
lutinat <lucas.dub92@gmail.com>
2023-10-23 15:40:55 +0200

    normals

--- pm_normals.py
+++ pm_normals.py
@@ -4,44 +4,19 @@ import obja
 import numpy as np
 import sys
 from tqdm import tqdm
+import random
 
 class Decimater(obja.Model):
     """
     A simple class that decimates a 3D model stupidly.
     """
 
-    def __init__(self, t):
+    def __init__(self):
         super().__init__()
-        self.t = t
         self.deleted_faces = set()
         self.deleted_vertices = set()
+        
 
-    def computeQs(self):
-        Qs = [np.zeros([4, 4]) for vertex in self.vertices]
-
-        for (face_index, face) in enumerate(self.faces):
-
-            x1, y1, z1 = self.vertices[face.a]
-            x2, y2, z2 = self.vertices[face.b]
-            x3, y3, z3 = self.vertices[face.c]
-
-            AB = np.array([x2 - x1, y2 - y1, z2 - z1])
-            AC = np.array([x3 - x1, y3 - y1, z3 - z1])
-
-            N = np.cross(AB, AC)  # Normal à AB et AC
-            N = N / np.linalg.norm(N)
-            a, b, c = N
-            d = -a * x1 - b * y1 - c * z1
-
-            p = np.array([a, b, c, d]).reshape(-1, 1)
-
-            Kp = p @ p.T
-
-            Qs[face.a] += Kp
-            Qs[face.b] += Kp
-            Qs[face.c] += Kp
-
-        return Qs
 
     def getValidPairs(self):
 
@@ -50,12 +25,13 @@ class Decimater(obja.Model):
         for (face_index, face) in tqdm(enumerate(self.faces)):
             f = [face.a, face.b, face.c]
             f.sort()
-
             validPairs.add((f[0], f[1]))
             validPairs.add((f[1], f[2]))
             validPairs.add((f[0], f[2]))
 
-        if self.t > 0:
+        t = 0
+
+        if t > 0:
             for (vertex1_index, vertex1) in tqdm(enumerate(self.vertices), total=len(self.vertices)):
                 for (vertex2_index, vertex2) in enumerate(self.vertices[vertex1_index + 1:], start=vertex1_index + 1):
                     #print(vertex1, vertex2, vertex1_index, vertex2_index)
@@ -64,69 +40,40 @@ class Decimater(obja.Model):
 
                     euclidean_distance = np.linalg.norm(v1 - v2)
 
-                    if euclidean_distance < self.t:
+                    if euclidean_distance < t:
                         validPairs.add((vertex1_index, vertex2_index))
 
 
         return list(validPairs)
-
-    def getError(self, Qs, pair):
-        a, b = pair
-        Q = Qs[a] + Qs[b]
-        
-        dQ = np.array([
-            [Q[0, 0], Q[0, 1], Q[0, 2], Q[0, 3]],
-            [Q[0, 1], Q[1, 1], Q[1, 2], Q[1, 3]],
-            [Q[0, 2], Q[1, 2], Q[2, 2], Q[2, 3]],
-            [0,      0,      0,      1]
-        ])
-
-        if np.linalg.det(dQ) > 0.001:
-            dQinv = np.linalg.inv(dQ)
-            v = dQinv @ (np.array([0, 0, 0, 1]).reshape(-1, 1))
-            e = (v.T @ Q @ v)[0, 0]
-        else:
-            v, e = self.minimize_error_on_line(Q, a, b)
-
-        return v, e
     
-    def minimize_error_on_line(self, Q, a, b):
-        v1 = np.array(self.vertices[a])
-        v2 = np.array(self.vertices[b])
-        # Calculer la direction de la droite v1 v2
-        direction = v2 - v1
-        direction /= np.linalg.norm(direction)
+    def getNormalDifference(self, n1, n2):
+        return np.linalg.norm(np.array(n1) - np.array(n2))
+    
+    def sorteByNormalDifference(self, l, normalDifferences):
+        return [x for _, x in sorted(zip(normalDifferences, l), key=lambda x: x[0])]
+    
+    def computeNormals(self):
+        vertex_normals = np.zeros((len(self.vertices), 3))
+        for vertex_index, _ in enumerate(self.vertices):
+            inc = 0
+            for _, face in enumerate(self.faces):
+                if vertex_index in [face.a, face.b, face.c]:
+                    x1, y1, z1 = self.vertices[face.a]
+                    x2, y2, z2 = self.vertices[face.b]
+                    x3, y3, z3 = self.vertices[face.c]
 
-        # Trouver le point sur la droite qui minimise l'erreur
-        v_min_error = None
-        min_error = float('inf')
+                    AB = np.array([x2 - x1, y2 - y1, z2 - z1])
+                    AC = np.array([x3 - x1, y3 - y1, z3 - z1])
 
-        for t in np.linspace(0, 1, 100):
-            v_test = (v1 + t * direction).reshape(-1, 1)
-            v_test = np.vstack((v_test, [1]))
-            e_test = (v_test.T @ Q @ v_test)[0, 0]
-
-            if e_test < min_error:
-                v_min_error = v_test
-                min_error = e_test
-
-        return v_min_error, min_error
-
-    def getErrors(self, Qs, validPairs):
-        errors = []
-        vs = []
-
-        for pair in tqdm(validPairs):
-            v, e = self.getError(Qs, pair)
-
-            vs.append(v)
-            errors.append(e)
-            
-        return errors, vs
-
-    def sorteByError(self, l, errors):
-        return [x for _, x in sorted(zip(errors, l), key=lambda x: x[0])]
-
+                    N = np.cross(AB, AC)  # Normal à AB et AC
+                    N = N / np.linalg.norm(N)
+                    
+                    vertex_normals[vertex_index] += N
+                    inc += 1
+            vertex_normals[vertex_index] /= inc
+        
+        return vertex_normals
+                    
     def contract(self, output):
         """
         Decimates the model stupidly, and write the resulting obja in output.
@@ -134,25 +81,23 @@ class Decimater(obja.Model):
         operations = []
         operations_face = []
 
-        print("Calcul Qs")
-        Qs = self.computeQs()
-
+        # compute normals
+        vertex_normals = self.computeNormals()
+        
         print("Calcul validPairs")
         validPairs = self.getValidPairs()
-
-        print("Calcul errors")
-        errors, vs = self.getErrors(Qs, validPairs)
-
-        validPairs = self.sorteByError(validPairs, errors)
-        vs = self.sorteByError(vs, errors)
-        errors = self.sorteByError(errors, errors)
-
-
         progress_bar = tqdm(total=len(validPairs), desc="Processing")
+        
+        print(validPairs[0])
         while len(validPairs) != 0:
-            v1, v2 = validPairs.pop(0)
-            v_bar = vs.pop(0)
-            errors.pop(0)
+            #Remove first key of validPairs and assign it to v1 v2
+            #Get list of the keys of validPairs
+            validPairsDist = [self.getNormalDifference(vertex_normals[pair[0]], vertex_normals[pair[1]]) for pair in validPairs]
+            validPairs = self.sorteByNormalDifference(validPairs, validPairsDist)
+            key = validPairs.pop(0)
+            v1 = key[0]
+            v2 = key[1]
+            v_bar = (np.array(self.vertices[v1]) + np.array(self.vertices[v2])) / 2
 
             # Remplacer les v2 en v1 + recalculer l'erreur
             for index, pair in enumerate(validPairs):
@@ -161,9 +106,7 @@ class Decimater(obja.Model):
                         validPairs[index] = (v1, pair[1]) if pair[1] > v1 else (pair[1], v1)
                     elif v2 == pair[1]:
                         validPairs[index] = (v1, pair[0]) if pair[0] > v1 else (pair[0], v1)                    
-
-                    # Calcul de l'erreur
-                    vs[index], errors[index] = self.getError(Qs, validPairs[index])      
+    
             
             for index_face, face in enumerate(self.faces):
                 if index_face not in self.deleted_faces:
@@ -175,38 +118,26 @@ class Decimater(obja.Model):
                         # Changer les faces v2ab -> v1ab
                         operations.append(('ef', index_face, face.clone()))
                         face.a, face.b, face.c = [v if v != v2 else v1 for v in [face.a, face.b, face.c]]
-
-            # Mise à jour de Qs
-            Qs[v1] = Qs[v1] + Qs[v2]
+            
 
             operations.append(('vertex', v2, self.vertices[v2].copy()))
             self.deleted_vertices.add(v2)
 
             # Editer v1 -> v_bar
             operations.append(("ev", v1, self.vertices[v1]))
-            self.vertices[v1] = v_bar[:-1].flatten().tolist()
+            self.vertices[v1] = v_bar.flatten().tolist()
 
             # Enlever les pairs en doubles
             validPairs_ = []
             validPairs_set = set()
-            vs_ = []
-            errors_ = []
 
             for index, pair in enumerate(validPairs):
                 if pair not in validPairs_set:
-                    validPairs_set.add(pair)
-                    validPairs_.append(pair)
-                    vs_.append(vs[index])
-                    errors_.append(errors[index])
+                    validPairs_set.add((pair[0], pair[1]))
+                    validPairs_.append((pair[0], pair[1]))
+   
 
             validPairs = validPairs_
-            vs = vs_
-            errors = errors_
-
-            # Re-tri de validPairs et vs selon errors
-            validPairs = self.sorteByError(validPairs, errors)
-            vs = self.sorteByError(vs, errors)
-            errors = self.sorteByError(errors, errors)
 
             progress_bar.update(1)
             progress_bar.total = len(validPairs)
@@ -238,14 +169,13 @@ def main():
     Runs the program on the model given as parameter.
     """
 
-    if len(sys.argv) != 2 and len(sys.argv) != 3:
-        print("Utilisation : python decimate2.py model t(optional)")
+    if len(sys.argv) != 2:
+        print("Utilisation : python decimate2.py model")
         exit(0)
 
     model_name = sys.argv[1]
-    t = 0 if len(sys.argv) == 2 else float(sys.argv[2])
     np.seterr(invalid='raise')
-    model = Decimater(t)
+    model = Decimater()
     model.parse_file(f'example/{model_name}.obj')
 
     with open(f'example/{model_name}.obja', 'w') as output:
